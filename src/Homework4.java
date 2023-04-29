70ad39e00e72bb43cad8ba1b9e9d5e5c0674c7f2
iShantaram <1agapovav987@gmail.com>
2023-04-29 20:12:39 +0300

    Init commit

---
+++ src/Homework4.java
@@ -0,0 +1,23 @@
+import transport.*;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        Auto lada = new Auto("Lada", "Granta", 1.6);
+        Auto audi = new Auto("Audi", "A8 50L TDI Quattro", 3.0);
+        Auto bmw = new Auto("BMW", "Z8", 3.0);
+
+        Bus mb = new Bus("Mercedes-Benz", "Tourismo", 10);
+        Bus man = new Bus("MAN", "Lion's Coach", 8);
+        Bus scania = new Bus("Scania", "Irizar I6", 12);
+
+        Track maz = new Track("МАЗ", "6312", 7.4);
+        Track kamaz = new Track("КаМАЗ", "4326", 10.8);
+        Track volvo = new Track("Volvo", "FH12", 12.8);
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+    }
+}
